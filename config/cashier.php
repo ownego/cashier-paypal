dba6fb3c0819c7119c7bc25d2eb8558cd1070534
Lam Duc Trung <vn.trungld@gmail.com>
2025-06-04 14:59:51 +0700

    Update: Change cashier path from 'cashier' to 'paypal'

--- config/cashier.php
+++ config/cashier.php
@@ -7,11 +7,10 @@ return [
     'client_secret' => env('PAYPAL_CLIENT_SECRET', ''),
     'app_id' => env('PAYPAL_APP_ID', 'APP-80W284485P519543T'),
     'webhook_id' => env('PAYPAL_WEBHOOK_ID', ''),
-
     'brand_name' => env('CASHIER_BRAND_NAME', 'Laravel'),
     'currency' => env('CASHIER_CURRENCY', 'USD'),
     'locale' => env('CASHIER_LOCALE', 'en-US'),
 
-    'path' => env('CASHIER_PATH', 'cashier'),
+    'path' => env('CASHIER_PATH', 'paypal'),
 
 ];
